c0ffa666a4f36e49bfcf74ce776b85339122559b
difabag <adif.maulana@gmail.com>
2022-06-22 00:01:51 +0700

    add default route

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => res.send('Selamat Datang di API Peta Pengetahuan Tradisional'))
 app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler

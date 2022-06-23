093812a71219ab8ea0eb917e6a51be9ac2dd64d0
difabag <adif.maulana@gmail.com>
2022-06-23 08:01:49 +0700

    turn back normal duration

--- controllers/admin.js
+++ controllers/admin.js
@@ -15,7 +15,7 @@ module.exports.login = async function (req, res) {
           { expiresIn: "1h" }
         );
         res.cookie("jwt", token, {
-          maxAge: 1 * 60 * 1000,
+          maxAge: 60 * 60 * 1000,
           httpOnly: true,
           secure: true,
           sameSite: "none",
